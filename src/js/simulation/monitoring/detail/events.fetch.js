bac84b3fd85ed628b00f66e8951a6d74d7d6201f
Mark Morgan <momipsl@ipsl.jussieu.fr>
2015-10-20 13:57:02 +0200

    Improved download of simulation jobs on detail page

--- src/js/simulation/monitoring/detail/events.fetch.js
+++ src/js/simulation/monitoring/detail/events.fetch.js
@@ -3,6 +3,29 @@
     // ECMAScript 5 Strict Mode
     "use strict";
 
+    var
+        // Returns a mapped job.
+        mapJob = function (i) {
+            return {
+                accountingProject: i[0],
+                executionEndDate: i[1],
+                executionStartDate: i[2],
+                isError: i[3],
+                isStartup: i[4],
+                jobUID: i[5],
+                postProcessingComponent: i[6],
+                postProcessingDate: i[7],
+                postProcessingDimension: i[8],
+                postProcessingFile: i[9],
+                postProcessingName: i[10],
+                simulationUID: i[11],
+                schedulerID: i[12],
+                submissionPath: i[13],
+                typeof: i[14],
+                warningDelay: i[15]
+            };
+        };
+
     // Controlled vocabularies loaded event handler.
     // @data     Data loaded from remote server.
     MOD.events.on("setup:cvDataLoaded", function (data) {
@@ -23,6 +46,9 @@
 
     // Event handler: page data downloaded.
     MOD.events.on("setup:pageDataDownloaded", function (ei) {
+        // Map tuples to JSON objects.
+        ei.jobList = _.map(ei.jobList, mapJob);
+
         // Parse data.
         MOD.parseSimulation(ei.simulation, ei.jobList);
 
