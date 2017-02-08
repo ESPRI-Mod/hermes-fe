ad40e602ede04703500a949b2056b238cedaaa0d
Mark Morgan <momipsl@ipsl.jussieu.fr>
2017-02-08 12:46:00 +0100

    version bump

--- src/js/app/main.js
+++ src/js/app/main.js
@@ -37,7 +37,7 @@ window.$ = window.$jq = jQuery.noConflict();
         title: "HERMES",
 
         // App version.
-        version : "1.2.0.0",
+        version : "1.2.0.1",
 
         // App copyright statement.
         copyrightYear: new Date().getFullYear(),
